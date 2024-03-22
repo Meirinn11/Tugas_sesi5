8ac57a396dd47b3881cdc8cef2bc2bdef5b5ce2b
Erin Nurfajrina <160722209+Meirinn11@users.noreply.github.com>
2024-03-22 10:11:59 +0700

    soal 1.py

--- soal 1.py
+++ soal 1.py
@@ -3,8 +3,8 @@
 #3. Aku cinta Indonesia
 #5. Aku cinta Indonesia
 print("no.1")
-a=1
-max=6
-while (a < max ):
-    print(a,end=". aku cinta Indonesia \n" )
-    a+=2
+a = 3
+num = 1
+for i in range (a):
+    print(f"{num}.Aku Cinta Indonesia")
+    num += 2
